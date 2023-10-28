513869dfa953fc4a0c66c5780c5ee40bd86a15bc
Yousef <yuossefbakier@gmail.com>
2023-10-28 14:13:45 +0200

    Add Stats Route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,18 @@ from flask import jsonify
 def status():
     """return a json say that everything is ok"""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """return the number of each objects by type"""
+    from models import storage
+    objects = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+
+    return jsonify(objects)
