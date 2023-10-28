3677e546fae2602d98e498f1e2be3778357ff17f
Yousef <yuossefbakier@gmail.com>
2023-10-28 13:07:22 +0200

    Add BluePrint and route status

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for Add Some route to Flask App"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """return a json say that everything is ok"""
+    return jsonify({'status': 'OK'})
