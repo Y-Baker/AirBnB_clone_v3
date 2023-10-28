8b3f35e616b0fb4ea5124e6e38296dd738f68f82
Yousef <yuossefbakier@gmail.com>
2023-10-28 10:16:07 +0200

    Add 2 Methods to Storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,16 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """A method to retrieve one object by its id"""
+        objects = self.all(cls).values()
+        for obj in objects:
+            if id and obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        objects = self.all(cls)
+        return len(objects)
