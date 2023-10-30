f0476fe0177f71a5fe06dc5e4188b72321f1cf7b
Yousef <yuossefbakier@gmail.com>
2023-10-30 14:40:38 +0200

    Fix Pep8

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,9 +4,10 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.place import Place
 from models.city import City
+from models.place import Place
 from models.user import User
+
 
 @app_views.route('/cities/<city_id>/places',
                  methods=['GET'], strict_slashes=False)
